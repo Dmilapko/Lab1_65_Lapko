7abb692c534103336365bbce626c513300805063
Dmilapko <68218175+Dmilapko@users.noreply.github.com>
2026-02-21 20:31:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1_65_Lapko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
