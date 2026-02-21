a0b74e70a41892aaa43fffca1cf4b36a5863a516
Dmilapko <68218175+Dmilapko@users.noreply.github.com>
2026-02-21 21:37:57 +0200

    Simple non crud storage and mapper

---
+++ AcademicService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_65_Lapko
+{
+    public class AcademicService
+    {
+        // Get all Subjects (Parent Level)
+        public List<Subject> GetAllSubjects()
+        {
+            var subjectEntities = MockStorage.Subjects;
+            var result = new List<Subject>();
+
+            foreach (var entity in subjectEntities)
+            {
+                result.Add(MapToDomain(entity));
+            }
+            return result;
+        }
+
+        // Get Details for a specific Subject (including loading sessions)
+        public Subject GetSubjectDetails(Guid subjectId)
+        {
+            var entity = MockStorage.Subjects.FirstOrDefault(s => s.Id == subjectId);
+            if (entity == null) return null;
+
+            var domainSubject = MapToDomain(entity);
+
+            var sessionEntities = MockStorage.Sessions.Where(s => s.SubjectId == subjectId).ToList();
+            domainSubject.Sessions = sessionEntities.Select(MapToDomain).ToList();
+
+            return domainSubject;
+        }
+
+        // Mappers
+        private Subject MapToDomain(SubjectEntity entity)
+        {
+            return new Subject
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                EctsCredits = entity.EctsCredits,
+                Area = entity.Area
+            };
+        }
+
+        private Session MapToDomain(SessionEntity entity)
+        {
+            return new Session
+            {
+                Id = entity.Id,
+                SubjectId = entity.SubjectId,
+                Date = entity.Date,
+                StartTime = entity.StartTime,
+                EndTime = entity.EndTime,
+                Topic = entity.Topic,
+                Type = entity.Type
+            };
+        }
+    }
+}
---
+++ MockStorage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_65_Lapko
+{
+    public static class MockStorage
+    {
+        public static List<SubjectEntity> Subjects { get; private set; }
+        public static List<SessionEntity> Sessions { get; private set; }
+
+        static MockStorage()
+        {
+            Subjects = new List<SubjectEntity>();
+            Sessions = new List<SessionEntity>();
+            SeedData();
+        }
+
+        private static void SeedData()
+        {
+            var sub1 = new SubjectEntity { Name = "C# Programming", EctsCredits = 5, Area = KnowledgeArea.Programming };
+            var sub2 = new SubjectEntity { Name = "Linear Algebra", EctsCredits = 4, Area = KnowledgeArea.Mathematics };
+            var sub3 = new SubjectEntity { Name = "Lollygagging", EctsCredits = 3, Area = KnowledgeArea.Humanities };
+
+            Subjects.AddRange([sub1, sub2, sub3]);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Sessions.Add(new SessionEntity
+                {
+                    SubjectId = sub1.Id,
+                    Topic = $"C# Lesson {i}",
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(i)),
+                    StartTime = new TimeOnly(10, 0),
+                    EndTime = new TimeOnly(11, 30),
+                    Type = i % 2 == 0 ? SessionType.Laboratory : SessionType.Lecture
+                });
+            }
+
+            Sessions.Add(new SessionEntity
+            {
+                SubjectId = sub2.Id,
+                Topic = "Matrices Intro",
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(1)),
+                StartTime = new TimeOnly(14, 0),
+                EndTime = new TimeOnly(15, 0),
+                Type = SessionType.Lecture
+            });
+            Sessions.Add(new SessionEntity
+            {
+                SubjectId = sub2.Id,
+                Topic = "Vectors",
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(3)),
+                StartTime = new TimeOnly(14, 0),
+                EndTime = new TimeOnly(15, 30),
+                Type = SessionType.Seminar
+            });
+        }
+    }
+}
