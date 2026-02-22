ef2f80ca71ad2a9abc40ffc238c4442b27a9d1b5
Dmilapko <68218175+Dmilapko@users.noreply.github.com>
2026-02-22 11:26:46 +0200

    Populate full subject in mapping

--- AcademicService.cs
+++ AcademicService.cs
@@ -33,20 +33,6 @@ namespace Lab1_65_Lapko
             return result;
         }
 
-        // Get Details for a specific Subject (including loading sessions)
-        public Subject GetSubjectDetails(Guid subjectId)
-        {
-            var entity = MockStorage.Subjects.FirstOrDefault(s => s.Id == subjectId);
-            if (entity == null) return null;
-
-            var domainSubject = MapToDomain(entity);
-
-            var sessionEntities = MockStorage.Sessions.Where(s => s.SubjectId == subjectId).ToList();
-            domainSubject.Sessions = sessionEntities.Select(MapToDomain).ToList();
-
-            return domainSubject;
-        }
-
         // Mappers
         private Subject MapToDomain(SubjectEntity entity)
         {
@@ -55,7 +41,11 @@ namespace Lab1_65_Lapko
                 Id = entity.Id,
                 Name = entity.Name,
                 EctsCredits = entity.EctsCredits,
-                Area = entity.Area
+                Area = entity.Area,
+                Sessions = MockStorage.Sessions
+                    .Where(s => s.SubjectId == entity.Id)
+                    .Select(MapToDomain)
+                    .ToList()
             };
         }
 
